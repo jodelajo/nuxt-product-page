e42e9982a418ef3ff273b0a2ea2af7bb18c1e470
jodelajo <jodelajo@gmail.com>
2021-12-12 11:34:08 +0100

    Display subgroups and add a filter for subgroups

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,33 @@
 <template>
   <div class="main-container">
     <main class="product-container">
-      <h1>Alle producten</h1>
-      <div class="">
-        <input v-model="toggle" type="checkbox" id="toggle" name="toggle" />
-        <label for="toggle">{{ toggle }}</label>
-        <select v-model="sort" name="sort" id="sort">
-          <option value="alphabetical">Sorteer op</option>
-          <option value="alphabetical">A-Z</option>
-          <option value="priceASC">Prijs oplopend</option>
-          <option value="priceDESC">Prijs aflopend</option>
-        </select>
+      <div>
+        <ul>
+          <li
+            v-for="group in Object.keys(subGroups)"
+            :key="group"
+            v-on:click="selectGroup(group)"
+          >
+            {{ group }} : {{ subGroups[group] }}
+          </li>
+        </ul>
       </div>
-      <div class="wrapper">
-        <ProductsCardDisplay :products="products" />
+      <div>
+        <h1>Alle producten</h1>
+
+        <div class="">
+          <input v-model="toggle" type="checkbox" id="toggle" name="toggle" />
+          <label for="toggle">{{ toggle }}</label>
+          <select v-model="sort" name="sort" id="sort">
+            <option value="alphabetical">Sorteer op</option>
+            <option value="alphabetical">A-Z</option>
+            <option value="priceASC">Prijs oplopend</option>
+            <option value="priceDESC">Prijs aflopend</option>
+          </select>
+        </div>
+        <div class="wrapper">
+          <ProductsCardDisplay :products="products" />
+        </div>
       </div>
     </main>
   </div>
@@ -28,6 +42,7 @@ export default {
     return {
       toggle: false,
       sort: "alphabetical",
+      displayGroup: "all",
     };
   },
   async fetch({ store }) {
@@ -35,6 +50,12 @@ export default {
       "https://a1-api.detailresult.nl/v1/assortmentcache/group/281/104?api_key=6d3a42a3-6d93-4f98-838d-bcc0ab2307fd"
     );
     store.commit("init", res.data);
+  },
+  methods: {
+    selectGroup(group) {
+      console.log("hallo", this);
+      this.displayGroup = group;
+    },
   },
   computed: {
     productFilter() {
@@ -50,6 +71,23 @@ export default {
           }
         };
         return isAProduct;
+      }
+    },
+    groupFilter() {
+      if (this.displayGroup === "all") {
+        const isAProduct = (item) => {
+          if (item.ProductID) {
+            return true;
+          }
+        };
+        return isAProduct;
+      } else {
+        const belongsToSelectedGroup = (item) => {
+          return item.WebSubGroups.some((group) => {
+            return group.Description === this.displayGroup;
+          });
+        };
+        return belongsToSelectedGroup;
       }
     },
     sortDirection() {
@@ -98,10 +136,36 @@ export default {
     },
     ...mapState({
       products(state) {
-        console.log(this.productFilter);
         return state.products
+          .filter(this.groupFilter)
           .filter(this.productFilter)
           .sort(this.sortDirection);
+      },
+      subGroups(state) {
+        // let groups = {
+        //   "Overige koekjes": 68,
+        //   "Biscuits": 7,
+        // }
+        let groups = {};
+        for (const product of state.products) {
+          // Als we een nieuwe subgroup tegenkomen, voeg het toe aan het object met description als key
+          // als de subgroep al in het object zit, dan doen we subgroep + 1
+          // groups = groups.concat(product.WebSubGroups)
+          for (const group of product.WebSubGroups) {
+            if (!groups[group.Description]) {
+              groups[group.Description] = 1;
+            } else if (groups[group.Description]) {
+              groups[group.Description]++;
+            }
+          }
+        }
+        console.log(groups);
+        // sorteer op groepen met de meeste producten
+        const sortedGroups = Object.fromEntries(
+          Object.entries(groups).sort(([, a], [, b]) => b - a)
+        );
+        console.log(sortedGroups);
+        return sortedGroups;
       },
     }),
   },
